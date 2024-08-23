eb4ad3f0f09c32a5f0cac48828ecea786869e646
Naufal Muafi <nmuafi1@gmail.com>
2024-08-23 20:06:37 +0700

    add: arm_connector and lower_arm control

--- controllers/obs_controller/obs_controller.py
+++ controllers/obs_controller/obs_controller.py
@@ -1,35 +1,42 @@
 import math
 from controller import Supervisor
 
+MAX_MOTOR_SPEED = 0.2
+
 # Create the Robot instance
 robot = Supervisor()
 
 # Get the time step of the current world
 timestep = int(robot.getBasicTimeStep())
 
+# List of names of the motors and sensors
+names = ["turret", "arm_connector", "lower_arm", "uppertolow", "scoop"]
+
 # Initialize motors for all wheels
-motors = [robot.getDevice(f"wheel{i}_motor") for i in range(1, 5)]
+wheel_motors = [robot.getDevice(f"wheel{i}_motor") for i in range(1, 5)]
 
-# Initialize turret motor and sensor
-turret_motor = robot.getDevice("turret_motor")
-turret_sensor = robot.getDevice("turret_sensor")
-
-# Enable the position sensor
-turret_sensor.enable(timestep)
+# Initialize motors and sensors for all joints as dictionaries
+motors = {name: robot.getDevice(f"{name}_motor") for name in names}
+sensors = {name: robot.getDevice(f"{name}_sensor") for name in names}
 
 # Set motors to velocity control mode
-for motor in motors:
+for wheel_motor in wheel_motors:
+    wheel_motor.setPosition(float("inf"))
+    wheel_motor.setVelocity(0.0)
+
+# Set motors to position and velocity control mode
+for motor in motors.values():
     motor.setPosition(float("inf"))
     motor.setVelocity(0.0)
 
-# Set turret motor to velocity control mode
-turret_motor.setPosition(float("inf"))
-turret_motor.setVelocity(0.0)
+# Enable the sensors
+for sensor in sensors.values():
+    sensor.enable(timestep)
 
 
-def run_all_motors(velocity):
-    for motor in motors:
-        motor.setVelocity(velocity)
+def run_all_wheels(velocity):
+    for wheel_motor in wheel_motors:
+        wheel_motor.setVelocity(velocity)
 
 
 # Function to calculate the shortest rotation direction and angle difference
@@ -46,19 +53,49 @@ def calculate_turret_movement(current_angle, target_angle):
 
 
 # Function to move turret to a specific angle in degrees
-def move_turret_to_angle(target_angle_degrees, turret_speed=0.2):
+def move_turret_to_angle(target_angle_degrees, turret_speed=MAX_MOTOR_SPEED):
     target_angle_radians = math.radians(
         target_angle_degrees
     )  # Convert target angle to radians
 
     # Get the current position of the turret
-    current_angle = turret_sensor.getValue()
+    current_angle = sensors["turret"].getValue()
 
     # Calculate the difference to the target position
     direction = calculate_turret_movement(current_angle, target_angle_radians)
 
     # move the turret to the target position
-    turret_motor.setVelocity(turret_speed * direction)
+    motors["turret"].setVelocity(turret_speed * direction)
+
+
+def move_arm_connector(
+    direction, min_position=-1.1, max_position=1.1, velocity=MAX_MOTOR_SPEED
+):
+    current_position = sensors["arm_connector"].getValue()
+
+    # Check if the motor is within the defined range
+    if min_position <= current_position <= max_position:
+        if direction == 0:
+            motors["arm_connector"].setVelocity(velocity)
+        elif direction == 1:
+            motors["arm_connector"].setVelocity(-velocity)
+    else:
+        motors["arm_connector"].setVelocity(0.0)
+
+
+def move_lower_arm(
+    direction, min_position=-0.27, max_position=0.27, velocity=MAX_MOTOR_SPEED
+):
+    current_position = sensors["lower_arm"].getValue()
+
+    # Check if the motor is within the defined range
+    if min_position <= current_position <= max_position:
+        if direction == 0:
+            motors["lower_arm"].setVelocity(velocity)
+        elif direction == 1:
+            motors["lower_arm"].setVelocity(-velocity)
+    else:
+        motors["lower_arm"].setVelocity(0.0)
 
 
 # Main loop:
@@ -66,18 +103,20 @@ start_time = robot.getTime()
 
 while robot.step(timestep) != -1:
     # Set velocity for the first 3 seconds, then stop
-    # duration = robot.getTime() - start_time
+    duration = robot.getTime() - start_time
 
     # if duration <= 3.0:
-    #     run_all_motors(1.0)
-    #     turret_motor.setVelocity(0.2)
+    # motors["arm_connector"].setVelocity(0.2)
+    # run_all_wheels(1.0)
+    # motors["turret"].setVelocity(0.2)
     # elif duration > 3.0 and duration < 5.0:
-    #     run_all_motors(0.0)
-    #     turret_motor.setVelocity(0.0)
+    # motors["arm_connector"].setVelocity(0.0)
+    # run_all_wheels(0.0)
+    # motors["turret"].setVelocity(0.0)
     # elif duration >= 5.0 and duration <= 8.0:
-    #     run_all_motors(-1.0)
+    #     run_all_wheelrs(-1.0)
     # elif duration > 8.0:
-    #     run_all_motors(0.0)
+    #     run_all_wheels(0.0)
 
-    move_turret_to_angle(300)
-    print(math.degrees(turret_sensor.getValue()))
+    move_lower_arm(1)
+    print(sensors["lower_arm"].getValue())
