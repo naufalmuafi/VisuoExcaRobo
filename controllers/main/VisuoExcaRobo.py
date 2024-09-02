e6e985b1c6503bc33d078cb1f7b12b8c25cf1cc0
naufalmuafi <naufalmuafi@mail.ugm.ac.id>
2024-09-02 13:44:57 +0700

    checkpoint: Training 5e6 steps Success.

--- controllers/main/VisuoExcaRobo.py
+++ controllers/main/VisuoExcaRobo.py
@@ -35,6 +35,9 @@ class VisuoExcaRobo:
 
         # Create the directories
         self.model_dir, self.log_dir = self.create_dir(self.model_name, self.log_name)
+        
+        # Get the current date in YYYYMMDD format
+        self.today_date = datetime.datetime.now().strftime("%Y%m%d")
 
     def extract_args(self, args) -> Tuple[str, int, str, str]:
         # extract the arguments
@@ -84,13 +87,10 @@ class VisuoExcaRobo:
             print("Please check the environment and try again.")
             return False
 
-    def train_PPO(self, batch_size=64, learning_rate=0.0003) -> None:
-        # Get the current date in YYYYMMDD format
-        today_date = datetime.datetime.now().strftime("%Y%m%d")
-
+    def train_PPO(self, batch_size=64, learning_rate=0.0003) -> None:        
         # Construct the model filename
-        model_filename = f"{self.model_dir}/{today_date}_ppo_{self.timesteps}_bs_{batch_size}_lr_{learning_rate:.0e}"
-        log_filename = f"{self.log_dir}/{today_date}_ppo_{self.timesteps}_bs_{batch_size}_lr_{learning_rate:.0e}"
+        model_filename = f"{self.model_dir}/{self.today_date}_ppo_{self.timesteps}_bs_{batch_size}_lr_{learning_rate:.0e}"
+        log_filename = f"{self.log_dir}/{self.today_date}_ppo_{self.timesteps}_bs_{batch_size}_lr_{learning_rate:.0e}"
 
         # use Proximal Policy Optimization (PPO) algorithm
         print("Training the model with PPO...")
@@ -110,42 +110,42 @@ class VisuoExcaRobo:
 
         return model_filename
 
-    def test_PPO(self, steps: int = 500, model_file: str = None) -> None:
+    def test_PPO(self, steps: int = 500, model_dir: str = None, model_file: str = None) -> None:
         # load the model
         try:
-            model = PPO.load(model_file, env=self.env)
+            model = PPO.load(model_dir, env=self.env)
         except FileNotFoundError:
             print(
                 "Model not found. Please train the model first/type your model name correctly."
             )
             return
-
         print("Load Model Successful")
 
+        # initialize the lists and variables
         step_list, reward_list = [], []
+        step, done = 0, False
 
-        # run a test
+        # reset the environment first
         obs, _ = self.env.reset()
-        for i in range(steps):
+        
+        # run the test
+        while not done:
             action, _states = model.predict(obs)
             obs, reward, done, _, _ = self.env.step(action)
             print(reward, done)
-            step_list.append(i)
+            step_list.append(step)
             reward_list.append(reward)
 
             if done:
                 obs, _ = self.env.reset()
-                print("Test Terminated.")
-
-            if i == steps - 1:
-                print("Test Successful")
+            
+            step += 1        
+        print("Test Success.")                
 
         # plot the reward
         plt.plot(step_list, reward_list)
-
         # save the plot
-        plt.savefig(f"{self.log_dir}/reward_plot.png")
-
+        plt.savefig(f"{self.log_dir}/test_reward_plot_{model_file}.png")
         # show the plot
         plt.show()
 
--- controllers/main/main.py
+++ controllers/main/main.py
@@ -7,6 +7,9 @@ LEARNING_RATE = 1e-4
 
 # main program
 if __name__ == "__main__":
+    # Define the temp directories
+    __modeldir = "models/"    
+    
     # Get the parsed arguments from parser.py
     args = parse_arguments(TIMESTEPS)
 
@@ -20,12 +23,13 @@ if __name__ == "__main__":
         print(f"Environment is ready: {robot.env}")
 
         # Train the model
-        model_file = robot.train_PPO(batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE)
+        # model_file = robot.train_PPO(batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE)
 
-        print("Training is finished, press `Y` for replay...")
-        robot.wait_for_y()
+        # print("Training is finished, press `Y` for replay...")
+        # robot.wait_for_y()
 
         # Test the environment
-        # model_file = "models/20240829_ppo_1000000_bs_1024_lr_1e-04"
+        model_file = "20240901_ppo_5000000_bs_1024_lr_1e-04"
+        model_train_dir = f"{__modeldir}{model_file}"
         print("Testing the Environment with Predicted Value")
-        robot.test_PPO(model_file=model_file)
+        robot.test_PPO(model_dir=model_train_dir, model_file=model_file)
